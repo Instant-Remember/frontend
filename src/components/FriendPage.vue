85648a72b33d80caf6d816ff9325fc7713e6cf8e
AYatsenko <a.yatsenko5320@gmail.com>
2024-04-23 13:52:27 +0300

    Отображение постов стороннего пользователя

--- src/components/FriendPage.vue
+++ src/components/FriendPage.vue
@@ -6,9 +6,9 @@
     </photo>
 
     <description>
-      <user>
+      <user class="userheader">
         <div class="test1">{{ friendInfo.first_name }} {{ friendInfo.last_name }}</div>
-        <div class="test2">{{ friendInfo.about }}</div>
+        <div class="test2" id="test">{{ friendInfo.about }}</div>
       </user>
       <target>
         <div>{{ countGoals() }}</div>
@@ -27,12 +27,41 @@
     </description>
   </div>
 
+  <div class="posts">
+    <div class="post" v-for="(post, index) in posts" :key="index">
+
+      <user class="userpost">
+        <img src="/src/assets/img/avatar.svg" alt="">
+        <div class="username">{{ friendInfo.first_name }} {{ friendInfo.last_name }}</div>
+      </user>
+
+      <div class="mainpost">
+        <div class="targetname">{{ getGoalName(post.goal_id) }}</div>
+        <div class="description">{{ post.text }}</div>
+      </div>
+
+      <progressBar class="progres"></progressBar>
+
+      <date>Неделю назад</date>
+      <society>
+        <button class="likes">
+          <img src="/src/assets/img/likes.svg" alt="">
+          <div class="like">1K</div>
+        </button>
+        <button class="comments"><img src="/src/assets/img/comments.svg" alt=""></button>
+      </society>
+    </div>
+  </div>
+
 </template>
 
 <script>
 import axios from 'axios';
+import progressBar from './progressBar.vue'
 
 export default {
+  components: { progressBar },
+
   props: {
     friendName: String
   },
@@ -41,7 +70,8 @@ export default {
       friendInfo: {},
       friendGoals: [],
       friendSubscribers: [],
-      friendSubscriptions: []
+      friendSubscriptions: [],
+      posts: [], // Массив для хранения постов пользователя
     };
   },
   created() {
@@ -49,6 +79,7 @@ export default {
     this.fetchFriendGoals();
     this.fetchFriendSubscribers();
     this.fetchFriendSubscriptions();
+    this.fetchUserPosts(); // Выполнение запроса на получение постов пользователя при загрузке компонента
   },
   methods: {
     fetchFriendInfo() {
@@ -75,6 +106,10 @@ export default {
     countGoals() { // Метод для подсчета количества целей пользователя
       return this.friendGoals.length;
     },
+    getGoalName(goalId) { // Новый метод для получения имени цели по её id
+            const goal = this.friendGoals.find(goal => goal.id === goalId);
+            return goal ? goal.name : 'Цель не найдена';
+        },
     fetchFriendSubscribers() {
       axios.get(`http://158.160.88.115:8000/profile/${this.friendName}/subscribers`)
         .then(response => {
@@ -85,7 +120,7 @@ export default {
           console.error('Ошибка при получении подписчиков:', error);
         });
     },
-    countSubscribers() { 
+    countSubscribers() {
       return this.friendSubscribers.length;
     },
     fetchFriendSubscriptions() {
@@ -98,8 +133,18 @@ export default {
           console.error('Ошибка при получении подписок:', error);
         });
     },
-    countSubscribtions() { 
+    countSubscribtions() {
       return this.friendSubscriptions.length;
+    },
+    fetchUserPosts() {
+      axios.get(`http://158.160.88.115:8000/profile/${this.friendName}/posts`)
+        .then(response => {
+          console.log('Посты друга:', response.data);
+          this.posts = response.data; // Заполнение массива постами из ответа сервера
+        })
+        .catch(error => {
+          console.error('Ошибка при получении постов:', error);
+        });
     },
   }
 }
@@ -142,7 +187,7 @@ photo {
   margin-left: 31px;
 }
 
-user {
+.userheader {
   width: 119px;
   height: 39px;
 
@@ -269,6 +314,188 @@ button {
   outline: none;
 
   margin-top: 14px;
+  cursor: pointer;
+}
+
+
+.post {
+  width: 757px;
+  height: auto;
+  margin-left: 397px;
+  margin-top: 16px;
+
+  border-radius: 20px;
+  background-color: #fff;
+
+  display: grid;
+  grid-template-columns: 69px 555px 133px;
+  grid-template-rows: 62px 53px 40px;
+}
+
+.userpost {
+  grid-row: 1;
+  grid-column: 1 / 2;
+
+  display: flex;
+
+  width: 624px;
+
+}
+
+user img {
+  width: 42px;
+  height: 42px;
+
+  margin: 16px 16px;
+}
+
+.username {
+  margin-top: 27px;
+  width: 118px;
+  height: 19px;
+
+
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  display: flex;
+  align-items: center;
+
+  color: #444444;
+  width: 550px;
+
+}
+
+.mainpost {
+  grid-row: 2;
+  grid-column: 2 / 3;
+
+  margin-left: 5px;
+}
+
+.targetname {
+
+  width: auto;
+  height: 18px;
+
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 18px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  /* Диплом/Акцентный */
+  color: #6133B4;
+
+
+  /* Inside auto layout */
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+
+}
+
+.description {
+
+  width: 608px;
+  height: auto;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #444444;
+
+  margin-top: 4px;
+}
+
+.progres {
+  grid-row: 3;
+  grid-column: 1 / 2;
+
+  display: flex;
+
+  width: 533px;
+  margin-top: 15px;
+}
+
+
+date {
+  grid-row: 1;
+  grid-column: 3;
+
+  margin-top: 27px;
+  margin-left: 7px;
+
+  width: 109px;
+  height: 19px;
+
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 200;
+  font-size: 16px;
+  line-height: 19px;
+  display: flex;
+  align-items: center;
+
+  color: #444444;
+}
+
+society {
+  grid-row: 3;
+  grid-column: 3;
+
+  display: flex;
+
+  margin-left: 34px;
+  margin-top: 3px;
+}
+
+.likes {
+  display: flex;
+  width: 40px;
+  height: 19px;
+
+  padding: 0;
+  background-color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+.like {
+  margin-left: 4px;
+
+  width: 18px;
+  height: 18px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 200;
+  font-size: 15px;
+  line-height: 18px;
+
+  display: flex;
+  align-items: center;
+
+  color: #444444;
+
+
+}
+
+.comments {
+  width: 18px;
+  height: 18px;
+  margin-left: 24px;
+  padding: 0;
+  background-color: #fff;
+  border: none;
   cursor: pointer;
 }
 </style>
--- src/components/headerPc.vue
+++ src/components/headerPc.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="headerPc">
-        <img src="/src/assets/img/logo.svg" alt="Лого" class="logo">
-        <searchLine></searchLine>
-        <div class="menu_buttons">
-            <p id="p1">Популярное</p>
-            <p id="p2">Для тебя</p>
-        </div>
-        <div class="user">
-            <img src="/src/assets/img/avatar.svg" alt="Me">
-            <p>{{ user.first_name }} {{ user.last_name }}</p>
-        </div>
+  <div class="headerPc">
+    <img src="/src/assets/img/logo.svg" alt="Лого" class="logo">
+    <div class="searchLine">
+      <img src="/src/assets/img/search.svg" alt="">
+      <input type="text" placeholder="Поиск">
     </div>
+    <div class="menu_buttons">
+      <p id="p1">Популярное</p>
+      <p id="p2">Для тебя</p>
+    </div>
+    <div class="user">
+      <img src="/src/assets/img/avatar.svg" alt="Me">
+      <p>{{ user.first_name }} {{ user.last_name }}</p>
+    </div>
+  </div>
 </template>
-  
+
 <script>
-import searchLine from './searchLine.vue'
 import axios from 'axios';
 
 export default {
-  components: {searchLine},
-
   data() {
     return {
       user: {}
@@ -39,16 +39,16 @@ export default {
           Authorization: `Bearer ${accessToken}`
         }
       })
-      .then(response => {
-        // Обработка успешного ответа
-        console.log('Информация о пользователе:', response.data);
-        this.user = response.data;
-      })
-      .catch(error => {
-        // Обработка ошибки
-        console.error('Ошибка:', error);
-        this.error = error.message;
-      });
+        .then(response => {
+          // Обработка успешного ответа
+          console.log('Информация о пользователе:', response.data);
+          this.user = response.data;
+        })
+        .catch(error => {
+          // Обработка ошибки
+          console.error('Ошибка:', error);
+          this.error = error.message;
+        });
     }
   }
 }
@@ -56,87 +56,112 @@ export default {
 
 <style scoped>
 * {
-    display: flex;
+  display: flex;
 }
 
 .headerPc {
-    width: 1440px;
-    height: 70px;
-    background-color: #fff9f9;
-    display: flex;
+  width: 1440px;
+  height: 70px;
+  background-color: #fff9f9;
+  display: flex;
 
-    margin-left: auto;
-    margin-right: auto;
+  margin-left: auto;
+  margin-right: auto;
 
 
-    align-items: center;
+  align-items: center;
 
-    width: 1440px;
+  width: 1440px;
 
 }
 
 .logo {
 
-    margin-left: 64px;
+  margin-left: 64px;
 
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .searchLine {
-    width: 332px;
-    margin-left: 272px;
+  width: 332px;
+  margin-left: 272px;
 }
 
 .menu_buttons {
-    margin-left: 88px;
+  margin-left: 88px;
 
-    width: 250px;
-    height: 19px;
-    display: flex;
+  width: 250px;
+  height: 19px;
+  display: flex;
 }
 
 p {
-    height: 19;
-    margin-top: 0;
+  height: 19;
+  margin-top: 0;
 
 
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 19px;
-    display: flex;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  display: flex;
 
-    color: #444444;
+  color: #444444;
 
-    cursor: pointer;
+  cursor: pointer;
 
-    white-space: nowrap;
+  white-space: nowrap;
 
 }
 
 #p2 {
-    padding-left: 84px;
+  padding-left: 84px;
 }
 
 .user {
-    width: 184px;
-    height: 42px;
+  width: 184px;
+  height: 42px;
 
-    margin-left: 124px;
+  margin-left: 124px;
 
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .user img {
-    width: 42px;
-    height: 42px;
+  width: 42px;
+  height: 42px;
 }
 
 .user p {
-    margin-top: 12px;
-    margin-left: 24px;
-    height: 19px;
-    font-size: 16px;
+  margin-top: 12px;
+  margin-left: 24px;
+  height: 19px;
+  font-size: 16px;
+}
+.searchLine input {
+    width: 77%;
+    background-color: #E0E6EF;
+    border: none;
+    padding-left: 12px;
+
+    font-size: 14px;
+    outline: none;
+}
+
+.searchLine img {
+    height: 20px;
+    width: 20px;
+
+    margin-top: 9px;
+    margin-left: 12px;
+}
+
+.searchLine{
+    height: 41px;
+    display: flex;
+    background-color: #E0E6EF;
+
+    border-radius: 20px;
 }
 </style>
